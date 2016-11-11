5747708d1587f95e3549f54d1d82f7052be8bf4d
niclin <bboyceo@hotmail.com>
2016-11-11 15:59:56 +0800

    Use rulers 0.0.2 routing

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require "rulers"
 
+$LOAD_PATH << File.join(File.dirname(__FILE__),
+                                    "..", "app",
+                                    "controllers")
 module BestQuotes
   class Application < Rulers::Application
   end
 end
+
+require "quotes_controller"
